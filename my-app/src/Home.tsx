0e08303dfc504d41c86396b7f19c01a259f4453f
Navigoo <navigoo@me.com>
2023-04-06 10:54:24 +0200

    push

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -13,11 +13,35 @@ function Home() {
 
         />
            <Section
-        title={'Hej'}
-        description={'Leasing starting at $349/mo'}
-        bgImg={'Homepage-Model-3-Desktop-LHD.jpeg'}
+        title={'Model Y'}
+        description={'Schedule a Demo Drive'}
+        bgImg={'Homepage-Model-Y-Global-Desktop.jpeg'}
         LButton={"View Inventory"}
         RButton={"Custom Order"}
+
+        />
+              <Section
+        title={'Model S'}
+        description={'Schedule a Demo Drive'}
+        bgImg={'Model-S-homepage-desktop.jpeg'}
+        LButton={"View Inventory"}
+        RButton={"Custom Order"}
+
+        />
+                 <Section
+        title={'Model X'}
+        description={'Schedule a Demo Drive'}
+        bgImg={'Homepage-Model-X-Desktop-LHD.jpeg'}
+        LButton={"View Inventory"}
+        RButton={"Custom Order"}
+
+        />
+                  <Section
+        title={'Solar Panels'}
+        description={'Lowest Cost Solar Panels in America'}
+        bgImg={'_25-HP-SolarPanels-D.jpeg'}
+        LButton={"Order Now"}
+        RButton={"Learn More"}
 
         />
 
